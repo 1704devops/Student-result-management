16d55efcd65c486a9580ae4c6f34b4b72d24febc
Kolawole Segun O <114000598+1704devops@users.noreply.github.com>
2023-01-07 22:02:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ mongoose.connect(dbURL)
 
     //listen for request
     app.listen(3000, () => {
-      console.log('server listening on port 3000');
+      console.log('server listening on port 5000');
     });
   }).catch(err => {
     console.log(err);
